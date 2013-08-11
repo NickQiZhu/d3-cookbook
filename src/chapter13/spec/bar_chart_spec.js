5db462cc66420b17fac67150a73736c7274b6dcc
Nick Zhu <nick.qi.zhu@gmail.com>
2013-08-11 09:42:48 -0400

    add data method

--- src/chapter13/spec/bar_chart_spec.js
+++ src/chapter13/spec/bar_chart_spec.js
@@ -1,21 +1,24 @@
 describe('BarChart', function () {
-    var div;
+    var div,
+        chart,
+        data = [{x: 0, y: 0},{x: 1, y: 3},{x: 2, y: 6}];
 
     beforeEach(function () {
         div = d3.select('body').append('div');
+        chart = BarChart(div);
     });
 
     afterEach(function () {
         div.remove();
     });
 
-    describe('render', function () {
-        var chart;
-
-        beforeEach(function () {
-            chart = BarChart(div);
+    describe('.data', function () {
+        it('should allow setting and retrieve chart data', function(){
+            expect(chart.data(data).data()).toBe(data);
         });
+    });
 
+    describe('.render', function () {
         describe('svg', function () {
             it('should generate svg', function () {
                 chart.render();
@@ -45,6 +48,9 @@ describe('BarChart', function () {
                 chart.render();
                 expect(svg().select('g.body').attr('transform')).toBe('translate(30,10)')
             });
+        });
+
+        describe('bars', function () {
         });
     });
 
--- src/chapter13/src/bar_chart.js
+++ src/chapter13/src/bar_chart.js
@@ -2,7 +2,8 @@ function BarChart(p) {
     var that = {};
 
     var _parent = p, _width = 500, _height = 350,
-        _margins = {top: 10, left: 30, right: 10, bottom: 30};
+        _margins = {top: 10, left: 30, right: 10, bottom: 30},
+        _data;
 
     that.render = function () {
         var svg = _parent
@@ -24,6 +25,12 @@ function BarChart(p) {
     that.height = function (h) {
         if (!arguments.length) return _height;
         _height = h;
+        return that;
+    };
+
+    that.data = function (d) {
+        if (!arguments.length) return _data;
+        _data = d;
         return that;
     };
 
