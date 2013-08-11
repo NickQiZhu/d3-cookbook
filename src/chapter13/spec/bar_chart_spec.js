3c8c996990c79b792b9a50d96e3bf4404308bab2
Nick Zhu <nick.qi.zhu@gmail.com>
2013-08-11 09:35:17 -0400

    add chart body

--- src/chapter13/spec/bar_chart_spec.js
+++ src/chapter13/spec/bar_chart_spec.js
@@ -10,24 +10,42 @@ describe('BarChart', function () {
     });
 
     describe('render', function () {
+        var chart;
+
+        beforeEach(function () {
+            chart = BarChart(div);
+        });
+
         describe('svg', function () {
-            var chart;
-
-            beforeEach(function () {
-                chart = BarChart(div);
-            });
-
             it('should generate svg', function () {
                 chart.render();
-                expect(svg().empty()).not.toBe(true);
+                expect(svg()).not.toBeEmpty();
             });
 
-            it('should set svg height and width', function () {
+            it('should set default svg height and width', function () {
+                chart.render();
+                expect(svg().attr('width')).toBe('500');
+                expect(svg().attr('height')).toBe('350');
+            });
+
+            it('should allow changing svg height and width', function () {
                 chart.width(200).height(150).render();
                 expect(svg().attr('width')).toBe('200');
                 expect(svg().attr('height')).toBe('150');
             });
-        })
+        });
+
+        describe('chart body', function () {
+            it('create body g', function () {
+                chart.render();
+                expect(svg().select('g.body')).not.toBeEmpty();
+            });
+
+            it('should translate to (left, top)', function () {
+                chart.render();
+                expect(svg().select('g.body').attr('transform')).toBe('translate(30,10)')
+            });
+        });
     });
 
     function svg() {
--- src/chapter13/src/bar_chart.js
+++ src/chapter13/src/bar_chart.js
@@ -1,24 +1,29 @@
 function BarChart(p) {
     var that = {};
 
-    var parent = p, width, height;
+    var _parent = p, _width = 500, _height = 350,
+        _margins = {top: 10, left: 30, right: 10, bottom: 30};
 
     that.render = function () {
-        parent
+        var svg = _parent
             .append("svg")
-            .attr("height", height)
-            .attr("width", width);
+            .attr("height", _height)
+            .attr("width", _width);
+
+        svg.append("g")
+            .attr("class", 'body')
+            .attr("transform", "translate(" + _margins.left + "," + _margins.top + ")");
     };
 
     that.width = function (w) {
-        if (!arguments.length) return width;
-        width = w;
+        if (!arguments.length) return _width;
+        _width = w;
         return that;
     };
 
     that.height = function (h) {
-        if (!arguments.length) return height;
-        height = h;
+        if (!arguments.length) return _height;
+        _height = h;
         return that;
     };
 
